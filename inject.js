12d3fdc1c89a420c989572d9d9b99369a627532d
Aaron Bieber <deftly@gmail.com>
2014-09-11 15:41:14 -0600

    remove un-needed bits

--- inject.js
+++ inject.js
@@ -1,14 +1,5 @@
 
-function inject( file ) {
-	var s = document.createElement('script');
-	s.src = chrome.extension.getURL(file);
-	console.log('injecting %s', file);
-	(document.head||document.documentElement).appendChild(s);
-}
-
-os = document.createElement('script'), 
-o = 'var bab.js';
-
-inject('bab.js');
-
-(document.head||document.documentElement).appendChild(os);
+var s = document.createElement('script');
+s.src = chrome.extension.getURL("bab.js");
+console.log('injecting %s', "bab.js");
+(document.head||document.documentElement).appendChild(s);
